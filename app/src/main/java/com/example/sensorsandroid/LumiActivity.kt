f91a7410d673aed88d80a96e158ca30a873dd4fa
PedroRicardo117 <pedro.ricardo575@gmail.com>
2019-11-11 11:40:49 -0300

    Enviando o deu tempo de fazer, terminar as telas e suas funcionalidades

---
+++ app/src/main/java/com/example/sensorsandroid/LumiActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sensorsandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LumiActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lumi)
+    }
+}
